ad741fea10f920e19b7c7b7ad1239e369d3a9bb5
Carlo <mcfernandez8@up.edu.ph>
2018-05-11 13:46:40 +0800

    partCommit

---
+++ src/main/java/FarmerAddController.java
@@ -0,0 +1,66 @@
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FarmerAddController implements Initializable {
+
+    DatabaseReference db;
+
+    @FXML
+    private TextField textUsername;
+    @FXML
+    private TextField textPassword;
+    @FXML
+    private TextField textName;
+    @FXML
+    private TextField textLocation;
+    @FXML
+    private TextField textFavCrop;
+    @FXML
+    private Label lblError;
+
+    private void okPressed(ActionEvent event) {
+        if(textUsername.getText() == null)
+            textUsername.setText("");
+        if(textPassword.getText() == null)
+            textPassword.setText("");
+        if(textName.getText() == null)
+            textName.setText("");
+        if(textLocation.getText() == null)
+            textLocation.setText("");
+        if(textFavCrop.getText() == null)
+            textFavCrop.setText("");
+
+        if (textUsername.getText().equals("") || textPassword.getText().equals("") || textName.getText().equals("") || textLocation.getText().equals("")|| textFavCrop.getText().equals("")){
+            lblError.setText("Incomplete Data");
+        } else {
+            String username, password, name, location, favCrop;
+            username = textUsername.getText();
+            password = textPassword.getText();
+            name = textName.getText();
+            location = textLocation.getText();
+            favCrop = textFavCrop.getText();
+
+            Farmer farmer = new Farmer(favCrop, location, name, password, username);
+            db.setValueAsync(username, farmer);
+
+        }
+    }
+    private void cancelPressed(ActionEvent event){
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        db = FirebaseDatabase.getInstance().getReference().child("Farmers");
+
+    }
+}
